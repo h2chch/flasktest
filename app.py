eb42c4a8e285e5e96d8bb7e1a3a3692e667c5ada
hendrah <hhidayat@gmail.com>
2025-10-24 16:23:22 +0700

    init2

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, request
 from flask import render_template
+import redis
 
 app = Flask(__name__)
-
 
 @app.route("/", methods=['POST', 'GET'])
 def login():
     name = request.form.get('name')
-    return render_template('main.html', person=name)
+    new_data = name
+
+    if name is not None :
+        r = redis.Redis(
+            host='redis-18405.c252.ap-southeast-1-1.ec2.redns.redis-cloud.com',
+            port=18405,
+            decode_responses=True,
+            username="admin",
+            password="Ehsanjaya18!",
+        )
+        new_data = new_data + ':' + str(r.get('data'))
+        r.set('data', new_data)
+
+    
+    return render_template('main.html', person=name, data=new_data)
